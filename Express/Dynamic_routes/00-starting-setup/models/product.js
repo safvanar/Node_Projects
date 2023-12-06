a3fec507d633950e6154bf5ba381b935ef3b6218
safvanar <safvananiyaram@gmail.com>
2023-12-06 22:07:58 +0530

    Implemented Delete functionality

--- Express/Dynamic_routes/00-starting-setup/models/product.js
+++ Express/Dynamic_routes/00-starting-setup/models/product.js
@@ -18,7 +18,8 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id,title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -26,13 +27,33 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString()
     getProductsFromFile(products => {
-      products.push(this);
+      if(this.id){
+        const existingProductIndex = products.findIndex(prod => prod.id === this.id)
+        const updatedProduct = [...products]
+        updatedProduct[existingProductIndex] = this
+        fs.writeFile(p, JSON.stringify(updatedProduct), err => {
+          console.log(err);
+        });
+      }else{
+        this.id = Math.random().toString()
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
+      }
+    });
+  }
+
+  static deleteProductById(id, cb){
+    getProductsFromFile(products => {
+      const deleteProductIndex = products.findIndex(prod => prod.id===id)
+      products = products.slice(0,deleteProductIndex).concat(products.slice(deleteProductIndex+1))
       fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
-    });
+      cb()
+    })
   }
 
   static fetchAll(cb) {
