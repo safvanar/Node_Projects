44add8054c6158d3c9c3fc592d83ec75248480ce
safvanar <safvananiyaram@gmail.com>
2023-12-08 19:25:40 +0530

    Implemented delete functionality using Database

--- Express/Dynamic_routes/00-starting-setup/controllers/admin.js
+++ Express/Dynamic_routes/00-starting-setup/controllers/admin.js
@@ -14,8 +14,11 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null, title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  product.save()
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -51,18 +54,24 @@ exports.postEditProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
-    });
-  });
+  Product.fetchAll()
+    .then(([products]) => {
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products'
+      });
+    })
+    .catch(err => console.log(err))
 };
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId
-  Product.deleteProductById(prodId, () => {
-    res.redirect('/admin/products')
-  })
+  Product.deleteProductById(prodId)
+    .then(
+      res.redirect('/admin/products')
+    )
+    .catch(err => {
+      console.log(err)
+    })
 }
--- Express/Dynamic_routes/00-starting-setup/models/product.js
+++ Express/Dynamic_routes/00-starting-setup/models/product.js
@@ -1,23 +1,5 @@
-const fs = require('fs');
-const path = require('path');
-
+const db = require('../util/database')
 const Cart = require('./cart')
-
-const p = path.join(
-  path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
-);
-
-const getProductsFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
-  });
-};
 
 module.exports = class Product {
   constructor(id,title, imageUrl, description, price) {
@@ -29,47 +11,21 @@ module.exports = class Product {
   }
 
   save() {
-    getProductsFromFile(products => {
-      if(this.id){
-        const existingProductIndex = products.findIndex(prod => prod.id === this.id)
-        const updatedProduct = [...products]
-        updatedProduct[existingProductIndex] = this
-        fs.writeFile(p, JSON.stringify(updatedProduct), err => {
-          console.log(err);
-        });
-      }else{
-        this.id = Math.random().toString()
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), err => {
-          console.log(err);
-        });
-      }
-    });
+    return db.execute(
+      'INSERT INTO products (title, price, description, imageUrl) VALUES (?,?,?,?)', 
+      [this.title, this.price, this.description, this.imageUrl]
+      );
   }
 
-  static deleteProductById(id, cb){
-    getProductsFromFile(products => {
-      // const deleteProductIndex = products.findIndex(prod => prod.id===id)
-      // products = products.slice(0,deleteProductIndex).concat(products.slice(deleteProductIndex+1))
-      const product = products.find(prod => prod.id === id)
-      const updatedProducts = products.filter(prod => prod.id !== id)
-      fs.writeFile(p, JSON.stringify(/*products*/updatedProducts), err => {
-        if(!err){
-          Cart.deleteProduct(id, product.price)
-        }
-      });
-      cb()
-    })
+  static deleteProductById(id){
+    return db.execute('DELETE FROM products WHERE products.id=?', [id])
   }
 
-  static fetchAll(cb) {
-    getProductsFromFile(cb);
+  static fetchAll() {
+    return db.execute('SELECT * FROM products')
   }
 
-  static findById(id, cb){
-    getProductsFromFile((products) => {
-      const product = products.find(p => p.id === id)
-      cb(product)
-    })
+  static findById(id){
+    return db.execute('SELECT * FROM products WHERE products.id=?', [id])
   }
 };
