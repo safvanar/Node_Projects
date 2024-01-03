87d311fc80db0b8a38c613486dd800806b859f7a
safvanar <safvananiyaram@gmail.com>
2024-01-03 16:32:03 +0530

    db file restored

---
+++ Expense_Tracker/utils/database.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_NAME,
+    // logging: false
+})
+
+sequelize.authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.')
+    })
+    .catch(err => {
+        console.log('Unable to connect to the database:', err)
+    })
+
+module.exports = sequelize
