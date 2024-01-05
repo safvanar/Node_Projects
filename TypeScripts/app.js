1631bab51c1ec4f7cde10217cebc297606915998
safvanar <safvananiyaram@gmail.com>
2024-01-05 19:47:40 +0530

    typescript learned

--- TypeScripts/app.js
+++ TypeScripts/app.js
@@ -1,12 +1,36 @@
-var num1Element = document.getElementById('num1');
-var num2Element = document.getElementById('num2');
-var addBtn = document.querySelector('button');
-function addfunction(num1, num2) {
-    return num1 + num2;
+"use strict";
+const num1Element = document.getElementById('num1');
+const num2Element = document.getElementById('num2');
+const addBtn = document.querySelector('button');
+const numResult /*Array<number>*/ = [];
+const textResult = [];
+function add(num1, num2) {
+    if (typeof num1 === 'number' && typeof num2 === 'number') {
+        return num1 + num2;
+    }
+    else if (typeof num1 === 'string' && typeof num2 === 'string') {
+        return num1 + ' ' + num2;
+    }
+    return +num1 + +num2;
 }
-addBtn.addEventListener('click', function () {
-    var num1 = num1Element.value;
-    var num2 = num2Element.value;
-    var result = addfunction(+num1, +num2);
-    console.log(result);
+function printResult(resultObj /* or ResultObj*/) {
+    console.log(resultObj.val);
+}
+addBtn.addEventListener('click', () => {
+    const num1 = num1Element.value;
+    const num2 = num2Element.value;
+    const result = add(+num1, +num2);
+    const stringResult = add(num1, num2);
+    numResult.push(result);
+    textResult.push(stringResult);
+    printResult({ val: result, timestamp: new Date() });
+    console.log(numResult, textResult);
+});
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('It Worked!');
+    }, 1000);
+});
+myPromise.then((result) => {
+    console.log(result.split(' '));
 });
--- TypeScripts/app.ts
+++ TypeScripts/app.ts
@@ -1,14 +1,48 @@
 const num1Element = document.getElementById('num1') as HTMLInputElement
 const num2Element = document.getElementById('num2') as HTMLInputElement
-const addBtn = document.querySelector('button')
+const addBtn = document.querySelector('button')!
 
-function addfunction(num1: number, num2: number){
-    return num1+num2;
+const numResult: number[] /*Array<number>*/ = []
+const textResult: string[] = []
+
+//type alias
+type NumberOrString = number | string
+type Result = { val: number; timestamp: Date }
+interface ResultObj {
+    val: number;
+    timestamp: Date
+}
+
+function add(num1: NumberOrString, num2: NumberOrString){
+    if(typeof num1 === 'number' && typeof num2 === 'number'){
+        return num1 + num2
+    }else if(typeof num1 === 'string' && typeof num2 === 'string'){
+        return num1 + ' '+ num2
+    }
+    return +num1 + +num2;
+}
+
+function printResult(resultObj: Result /* or ResultObj*/){
+    console.log(resultObj.val)
 }
 
 addBtn.addEventListener('click', () => {
     const num1 = num1Element.value
     const num2 = num2Element.value
-    const result = addfunction(+num1, +num2)
-    console.log(result)
+    const result = add(+num1, +num2)
+    const stringResult = add(num1, num2)
+    numResult.push(result as number)
+    textResult.push(stringResult as string)
+    printResult({val: result as number, timestamp: new Date()})
+    console.log(numResult, textResult)
+})
+
+const myPromise = new Promise<string>((resolve, reject) => {
+    setTimeout(() => {
+        resolve('It Worked!')
+    }, 1000)
+})
+
+myPromise.then((result) => {
+    console.log(result.split(' '))
 })
