c2f32daf9e8485cc39d38b4428faa8a299644838
safvanar <safvananiyaram@gmail.com>
2024-01-05 21:48:36 +0530

    simple node todo app api using typescript

---
+++ TypeScripts_Node/routes/todos.ts
@@ -0,0 +1,38 @@
+import { Router } from 'express'
+
+import { todo } from '../models/todo'
+
+let todos: todo[] = []
+
+const router = Router()
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({todos: todos})
+})
+
+router.post('/todo', (req, res, next) => {
+    const newTodo: todo = {
+        id: new Date().toISOString(),
+        text: req.body.text
+    }
+    todos.push(newTodo)
+    return res.status(200).json({message: 'added new todo!', todo: newTodo, todos: todos})
+})
+
+router.put('/todo/:todoId', (req, res, next) => {
+    const todoId = req.params.todoId
+    const todoIndex = todos.findIndex(todo => todo.id === todoId)
+    if(todoIndex >= 0){
+        todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text}
+        return res.status(200).json({message: 'updated todos!', todos: todos})
+    }
+    res.status(403).json({message: 'could not find todo for this id!'})
+})
+
+router.delete('/todo/:todoId', (req, res, next) => {
+    const todoId = req.params.todoId
+    todos = todos.filter(todoItem => todoItem.id !== todoId)
+    res.status(200).json({message: 'todo deleted!', todos: todos})
+})
+
+export default router 
